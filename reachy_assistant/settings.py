46a48d68cf238cf5e7ee7886c7e023a499f0af3d
Jennifer Cwagenberg <candiedcode@gmail.com>
2026-04-26 16:40:26 -0400

    chore: update __inits__ to trigger cron job registration

--- reachy_assistant/settings.py
+++ reachy_assistant/settings.py
@@ -6,7 +6,7 @@ class Settings(pydantic_settings.BaseSettings):
 
     reachy_api_url: str = "http://localhost"
     """Address of the Reachy API. By default, it is set to `http://localhost`."""
-    reachy_api_port: int = 8791
+    reachy_api_port: int = 8790
     """Port of the Reachy API. By default, it is set to `8790`."""
 
     @property
