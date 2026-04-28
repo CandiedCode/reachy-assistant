8f8584973f38584d70d7f91fd12963f0962d5281
Jennifer Cwagenberg <candiedcode@gmail.com>
2026-04-28 13:25:35 -0400

    chore: update logging

--- reachy_assistant/__init__.py
+++ reachy_assistant/__init__.py
@@ -1,13 +1,5 @@
 """Reachy Assistant package."""
 
-import logging
-import sys
+from reachy_assistant.log_config import configure_logging
 
-# Ensure root logger has a handler for reachy_assistant logs
-# Do this even if handlers exist, to ensure we always have output
-root_logger = logging.getLogger()
-if not root_logger.handlers:
-    handler = logging.StreamHandler(sys.stderr)
-    handler.setFormatter(logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s"))
-    root_logger.addHandler(handler)
-    root_logger.setLevel(logging.INFO)
+configure_logging()
--- reachy_assistant/log_config.py
+++ reachy_assistant/log_config.py
@@ -5,11 +5,8 @@ import logging
 
 def configure_logging() -> None:
     """Configure the root logger with basic settings."""
-    # Force set the root logger level - basicConfig() may have already been called
-    logging.root.setLevel(logging.INFO)
-
-    # Ensure we have a handler
-    if not logging.root.handlers:
-        handler = logging.StreamHandler()
-        handler.setFormatter(logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s"))
-        logging.root.addHandler(handler)
+    logging.basicConfig(
+        level=logging.INFO,
+        format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
+        handlers=[logging.StreamHandler()],
+    )
